50cef139bea70090a46433f915ea8ee410164962
Benjamin Wilhelm <benjamin.wilhelm@uni-konstanz.de>
2019-05-17 12:44:36 +0200

    WIP: Switch to miniconda for faster prototyping

--- org.knime.python2.envconfigs/buildscripts/install-anaconda.sh
+++ org.knime.python2.envconfigs/buildscripts/install-anaconda.sh
@@ -4,15 +4,15 @@ set -x
 cd ~/python
 if [[ ! -d ~/anaconda3 ]]; then
         if [[ "$(uname -s)" == "Darwin" ]]; then
-                # INST_FILE=Miniconda3-4.4.10-MacOSX-x86_64.sh
-                INST_FILE=Anaconda3-5.1.0-MacOSX-x86_64.sh
+                INST_FILE=Miniconda3-4.4.10-MacOSX-x86_64.sh
+                # INST_FILE=Anaconda3-5.1.0-MacOSX-x86_64.sh
         else
-                # INST_FILE=Miniconda3-4.4.10-Linux-x86_64.sh
-                INST_FILE=Anaconda3-5.1.0-Linux-x86_64.sh
+                INST_FILE=Miniconda3-4.4.10-Linux-x86_64.sh
+                # INST_FILE=Anaconda3-5.1.0-Linux-x86_64.sh
         fi
 
-        # wget -qN https://repo.anaconda.com/miniconda/$INST_FILE
-        wget -qN https://repo.anaconda.com/archive/$INST_FILE
+        wget -qN https://repo.anaconda.com/miniconda/$INST_FILE
+        # wget -qN https://repo.anaconda.com/archive/$INST_FILE
         bash $INST_FILE -b -p ~/anaconda3
         rm $INST_FILE
 fi
